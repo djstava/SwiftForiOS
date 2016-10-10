4edd53f804b08ef6d14e00070b47cbf84a1d1717
djstava <djstava@gmail.com>
2016-10-10 23:37:13 +0800

    add ios first sight

---
+++ FirstSight/FirstSight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstSight
+//
+//  Created by djstava on 9/19/16.
+//  Copyright © 2016 djstava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirstSight/FirstSight/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  FirstSight
+//
+//  Created by djstava on 9/19/16.
+//  Copyright © 2016 djstava. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"Hello world!");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
